9074eb88ba4e49381d1d8efd0caaa0632bd5bb59
Zerbaib <81523999+Zerbaib@users.noreply.github.com>
2021-08-30 08:23:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-import tkinter
+import sys
 import os
+import tkinter
 from tkinter import *
+
+def CreateQRCode():
+    os.system('create_qrcode.py')
+
+def ReadQRCode():
+    os.system('read_qrcode.py')
 
 window = Tk()
 window.title("QRCode - by Zerbaib")
 window.geometry("720x480")
 window.minsize(480, 360)
-window.iconbitmap("C:\Hugo\Python\qrcode\logo.ico")
+window.iconbitmap("logo.ico")
 window.config(background="#9EDBF8")
 frame = Frame(window, bg="#9EDBF8")
 
@@ -15,8 +22,8 @@ label_subtitle = Label(frame, text="QRCode est un logiciel qui permet de cree et
 label_title.pack()
 label_subtitle.pack()
 
-cr_qr = Button(frame, text="Pour cree un qrcode click moi", font=("Courrier", 25), bg="white", fg="#9EDBF8")
-re_qr = Button(frame, text="Pour lire un qrcode click moi", font=("Courrier", 25), bg="white", fg="#9EDBF8")
+cr_qr = Button(frame, text="Pour cree un qrcode click moi", font=("Courrier", 25), bg="white", fg="#9EDBF8",command= CreateQRCode)
+re_qr = Button(frame, text="Pour lire un qrcode click moi", font=("Courrier", 25), bg="white", fg="#9EDBF8",command= ReadQRCode)
 cr_qr.pack(pady=25, fill=X)
 re_qr.pack(pady=25, fill=X)
 
